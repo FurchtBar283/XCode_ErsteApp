f28768708d225a9a8dd199c8b86b1a86dc16164d
Michael Stroh <schleppi@michael-mbp.fritz.box>
2015-12-28 14:53:54 +0100

    Added another view reachable by a sesueway. The view includes a slider and a switch with value-output-labels.

---
+++ ErsteApp/SliderViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SliderViewController.swift
+//  ErsteApp
+//
+//  Created by Michael Stroh on 28.12.15.
+//  Copyright © 2015 Michael Stroh. All rights reserved.
+//
+
+import UIKit
+
+class SliderViewController: UIViewController {
+    @IBOutlet weak var sliderTextfieldOutlet: UILabel!
+    @IBOutlet weak var switchTextfieldOutlet: UILabel!
+    @IBOutlet weak var switchTextfieldOutletTrue: UILabel!
+    
+    @IBAction func sliderChanged(sender: UISlider) {
+        let sliderValue = sender.value
+        //print(sliderValue)
+        sliderTextfieldOutlet.text = NSString(format: "%.2f", sliderValue) as String
+    }
+    
+    @IBAction func switchChanged(sender: UISwitch) {
+        let switchValueAsString = BoolToString(sender.on)
+        //print(switchValueAsString)
+        if switchValueAsString == "true" {
+            switchTextfieldOutletTrue.text = switchValueAsString
+            switchTextfieldOutlet.text = ""
+        } else {
+            switchTextfieldOutlet.text = switchValueAsString
+            switchTextfieldOutletTrue.text = ""
+        }
+    }
+    
+    func BoolToString(b: Bool)->String { return b.description ?? "<None>"}
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
